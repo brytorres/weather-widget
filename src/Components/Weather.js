294f38a1b7cb656453ddf57b55a799767c20d47b
brytorres <bryan@torvel.us>
2019-01-23 20:17:50 -0500

    Cleanup and prep

---
+++ src/Components/Weather.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.scss';
+
+class Weather extends Component {
+    render() {
+        return (
+            <div className="weather">
+
+            </div>
+        );
+    }
+}
+
+export default Weather;
